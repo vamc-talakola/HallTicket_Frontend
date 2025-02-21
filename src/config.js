4fd128040b05c687cdf7b3dc2f7e9b6fca352051
goutham3578 <gouthamvenkat2002@gmail.com>
2025-02-21 23:28:39 +0530

    Update backend base URL to production environment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 //add the base url of the backend and export it
-const BASE_URL = "http://localhost:4000";
-// const BASE_URL = "https://hall-ticket-backend.vercel.app";
+// const BASE_URL = "http://localhost:4000";
+const BASE_URL = "https://hall-ticket-backend.vercel.app";
 
 export default BASE_URL;
